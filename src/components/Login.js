255c7dba2058512a2b403306a309acaaa71000da
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2023-01-08 21:36:52 +0100

    repaired navigate after login

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
   const navigate = useNavigate();
   const location = useLocation();
-  const from = location.state?.from?.pathname || "/weaProjectClient";
+  const from = location.state?.from?.pathname || "/";
 
   const userRef = useRef();
   const errRef = useRef();
